fc41ff298d8b0ea4e818e1cc386f8687244d7594
langtaowu <langtaowu@tencen.com>
2023-03-18 19:35:44 +0800

    fix: channel's queue should be shared

--- src/core/channel.cpp
+++ src/core/channel.cpp
@@ -27,17 +27,18 @@ ChanTrait::ChanTrait(size_t capacity) : _capacity(capacity) {
   this->_mutex = std::make_shared<SpinLock>();
   this->_block_readers = std::make_shared<BlockSet>();
   this->_block_writers = std::make_shared<BlockSet>();
+  this->_queue = std::make_shared<std::queue<std::any>>();
 }
 
 auto ChanTrait::_send(std::any&& value) -> Async<void> {
   auto defer = [this]() { this->_mutex->unlock(); };
   while (true) {
     this->_mutex->lock();
-    DEBUG("[TH-{%u}]: send: queue_size=%d", std::this_thread::get_id(), this->_queue.size());
-    if (this->_queue.size() == this->_capacity) {
+    DEBUG("[TH-{%u}]: send: queue_size=%d", std::this_thread::get_id(), this->_queue->size());
+    if (this->_queue->size() == this->_capacity) {
       co_await Context::current().wait(this->_block_writers.get(), defer);
     } else {
-      this->_queue.push(std::move(value));
+      this->_queue->push(std::move(value));
       Context::current().notify({this->_block_readers.get(), this->_block_writers.get()}, defer);
       co_return;
     }
@@ -48,30 +49,39 @@ auto ChanTrait::_recv() -> Async<std::any> {
   auto defer = [this]() { this->_mutex->unlock(); };
   while (true) {
     this->_mutex->lock();
-    DEBUG("[TH-{%u}]: recv: queue_size=%d", std::this_thread::get_id(), this->_queue.size());
-    if (this->_queue.size() == 0) {
+    DEBUG("[TH-{%u}]: recv: queue_size=%d", std::this_thread::get_id(), this->_queue->size());
+    if (this->_queue->size() == 0) {
       co_await Context::current().wait(this->_block_readers.get(), defer);
     } else {
-      auto res = std::move(this->_queue.front());
-      this->_queue.pop();
+      auto res = std::move(this->_queue->front());
+      this->_queue->pop();
       Context::current().notify({this->_block_readers.get(), this->_block_writers.get()}, defer);
       co_return std::move(res);
     }
   }
 }
 
-auto ChanTrait::_send_nowait(std::any&& value) -> void {
+auto ChanTrait::_send_nowait(std::any&& value) -> bool {
   auto defer = [this]() { this->_mutex->unlock(); };
   this->_mutex->lock();
-  this->_queue.push(std::move(value));
+  if (this->_queue->size() == this->_capacity) {
+    defer();
+    return false;
+  }
+  this->_queue->push(std::move(value));
   Context::current().notify({this->_block_readers.get(), this->_block_writers.get()}, defer);
+  return true;
 }
 
 auto ChanTrait::_recv_nowait() -> std::any {
   auto defer = [this]() { this->_mutex->unlock(); };
   this->_mutex->lock();
-  auto res = std::move(this->_queue.front());
-  this->_queue.pop();
+  if (this->_queue->empty()) {
+    defer();
+    return std::any{};
+  }
+  auto res = std::move(this->_queue->front());
+  this->_queue->pop();
   Context::current().notify({this->_block_readers.get(), this->_block_writers.get()}, defer);
   return res;
 }
--- src/util/log.h
+++ src/util/log.h
@@ -8,7 +8,7 @@
 
 #ifdef USE_DEBUG
 #undef USE_DEBUG
-#define DEBUG(fmt, ...) printf("%s -> %s() [line: %d] " fmt "\n", __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__)
+#define DEBUG(fmt, ...) printf("%s -> %s() [line: %d] " fmt "\n", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #else
 #define DEBUG(fmt, ...)
 #endif
