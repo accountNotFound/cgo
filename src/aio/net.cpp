ee16b9d9d3609c941fdaa62c5c6bc1e4af6ae15b
langtaowu <langtaowu@tencen.com>
2023-06-27 10:14:05 +0000

    fix: tcp server accept

--- src/aio/net.cpp
+++ src/aio/net.cpp
@@ -27,9 +27,18 @@ TcpServer::TcpServer(size_t port) {
 
   // TODO: error handle
   this->_fd = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-  set_fd_nonblock(this->_fd);
-  ::bind(this->_fd, (::sockaddr*)(&this->_svr_addr), sizeof(this->_svr_addr));
-  ::listen(this->_fd, ListenCapacity);
+  if (int(this->_fd) < 0) {
+    exit(1);
+  }
+  if (set_fd_nonblock(this->_fd) < 0) {
+    exit(1);
+  }
+  if (::bind(this->_fd, (::sockaddr*)(&this->_svr_addr), sizeof(this->_svr_addr)) < 0) {
+    exit(1);
+  }
+  if (::listen(this->_fd, ListenCapacity) < 0) {
+    exit(1);
+  }
 
   Context::current().handler().add(this->_fd, Event::IN,
                                    [this](Event event) { this->_chan.send_nowait(std::move(event)); });
@@ -44,13 +53,19 @@ TcpServer::~TcpServer() {
 auto TcpServer::accept() -> Async<Connection> {
   ::sockaddr_in cli_addr = {};
   ::socklen_t sin_size = sizeof(::sockaddr_in);
-  co_await this->_chan.recv();
 
-  // TODO: error handle
-  Fd conn_fd = ::accept(this->_fd, (::sockaddr*)(&cli_addr), &sin_size);
-  set_fd_nonblock(conn_fd);
-
-  co_return Connection(conn_fd);
+  while (true) {
+    Fd conn_fd = ::accept(this->_fd, (::sockaddr*)(&cli_addr), &sin_size);
+    if (int(conn_fd) < 0) {
+      co_await this->_chan.recv();
+      continue;
+    }
+    // TODO: error handle
+    if (set_fd_nonblock(conn_fd) < 0) {
+      exit(1);
+    }
+    co_return Connection(conn_fd);
+  }
 }
 
 class Connection::Impl {
@@ -70,6 +85,7 @@ class Connection::Impl {
   ~Impl() {
     Context::current().handler().del(this->_fd);
     ::close(this->_fd);
+    DEBUG("tcp connection fd=%lu closed", this->_fd);
   }
   auto recv() -> Async<std::string> {
     co_await this->_chan_in.recv();
@@ -91,7 +107,7 @@ class Connection::Impl {
   auto send(const std::string& data) -> Async<void> {
     for (int i = 0; i < data.size(); i += BufferSize) {
       int nbytes = ::write(this->_fd, data.data() + i, std::min(BufferSize, data.size() - i));
-      DEBUG("copnnection fd=%lu send %d bytes", this->_fd, nbytes);
+      DEBUG("connection fd=%lu send %d bytes", this->_fd, nbytes);
       if (nbytes <= 0) {
         // TODO: error handle if nbytes==-1
         this->_ev_listening = Event::IN | Event::OUT | Event::ONESHOT;
@@ -116,6 +132,23 @@ Connection::~Connection() = default;
 auto Connection::recv() -> Async<std::string> { co_return (co_await this->_impl->recv()); }
 
 auto Connection::send(const std::string& data) -> Async<void> { co_await this->_impl->send(data); }
+
+TcpClient::TcpClient(const std::string& host, size_t port) {
+  this->_svr_addr.sin_family = AF_INET;
+  this->_svr_addr.sin_addr.s_addr = ::htonl(INADDR_ANY);
+  this->_svr_addr.sin_port = ::htons(port);
+}
+
+TcpClient::~TcpClient() = default;
+
+auto TcpClient::connect() -> Async<Connection> {
+  // TODO: error handle
+  Fd conn_fd = ::socket(AF_INET, SOCK_STREAM, 0);
+  set_fd_nonblock(conn_fd);
+  ::connect(conn_fd, (::sockaddr*)(&this->_svr_addr), sizeof(this->_svr_addr));
+
+  co_return Connection(conn_fd);
+}
 
 }  // namespace cgo::impl
 #endif
