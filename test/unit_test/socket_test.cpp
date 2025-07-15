92aa8ffcba529d0e53064d89bccb79d9a31bbe04
wlt <wlt15@tsinghua.org.cn>
2025-07-15 19:08:01 +0800

    test: udp benchmark

--- test/unit_test/socket_test.cpp
+++ test/unit_test/socket_test.cpp
@@ -24,7 +24,7 @@ struct Config {
 
   size_t test_duration_sec = 10;
 
-  std::string str() {
+  std::string str() const {
     std::ostringstream oss;
     oss << "socket timeout ms: " << sock_timeout_ms << "\n";
     oss << "server thread: " << svr_ctx_threads << "\n";
@@ -346,13 +346,202 @@ void tcp_benchmark_test(Config& conf) {
 }
 
 TEST(socket, tcp_bench) {
-  for (int i = 0; i < 4; ++i) {
-    Config conf;
-    conf.cli_num = 1000;
-    conf.cli_ctx_threads = i + 1;
-    conf.svr_ctx_threads = i + 1;
-    conf.test_duration_sec = 10;
-    tcp_benchmark_test(conf);
-    std::cout << "\n\n";
+  Config conf;
+  conf.test_duration_sec = 10;
+  conf.svr_port = 8080;
+  conf.sock_timeout_ms = 2000;
+  conf.cli_num = 2000;
+  conf.cli_interval_ms = 50;
+  conf.cli_ctx_threads = 1;
+  conf.svr_ctx_threads = 1;
+  conf.req_pkg_nbytes = 1024;
+  conf.res_pgk_nbytes = 1024;
+  tcp_benchmark_test(conf);
+  std::cout << "\n\n";
+}
+
+cgo::Coroutine<void> udp_client(const Config& conf, Metric& metric, std::atomic<size_t>& wg) {
+  auto& ctx = cgo::this_coroutine_ctx();
+  auto sock = cgo::Socket::create(ctx, cgo::Socket::Protocol::UDP, cgo::Socket::AddressFamily::IPv4);
+  auto guard = cgo::defer([&sock, &wg]() {
+    sock.close();
+    wg.fetch_add(1);
+  });
+  sock.bind("0.0.0.0", 0);
+  auto end_tp = std::chrono::steady_clock::now() + std::chrono::seconds(conf.test_duration_sec);
+
+  bool disconnect = true;
+  std::string svr_ip;
+  uint16_t svr_port;
+  while (std::chrono::steady_clock::now() < end_tp) {
+    co_await cgo::sleep(ctx, std::chrono::milliseconds(conf.cli_interval_ms));
+    if (disconnect) {
+      metric.conn_total_num.inc();
+      auto begin = std::chrono::steady_clock::now();
+      if (auto ok = co_await sock.sendto("1", conf.svr_ip, conf.svr_port); !ok) {
+        metric.conn_error_num.inc();
+        continue;
+      }
+      if (auto res = co_await sock.recvfrom(1, std::chrono::milliseconds(conf.sock_timeout_ms)); !res) {
+        metric.conn_error_num.inc();
+        continue;
+      } else {
+        auto end = std::chrono::steady_clock::now();
+        metric.conn_latency_ms.update((end - begin).count() / 1e6);
+        auto& [data, source] = *res;
+        svr_ip = source.first;
+        svr_port = source.second;
+        disconnect = false;
+      }
+    }
+    {
+      metric.send_total_num.inc();
+      auto req = generate_test_data(conf.req_pkg_nbytes);
+      auto begin = std::chrono::steady_clock::now();
+      auto ok = co_await sock.sendto(req, svr_ip, svr_port);
+      if (ok) {
+        auto end = std::chrono::steady_clock::now();
+        metric.send_latency_ms.update((end - begin).count() / 1e6);
+      } else {
+        metric.send_error_num.inc();
+        disconnect = true;
+        continue;
+      }
+    }
+    {
+      metric.recv_total_num.inc();
+      auto begin = std::chrono::steady_clock::now();
+      auto res = co_await sock.recvfrom(conf.res_pgk_nbytes, std::chrono::milliseconds(conf.sock_timeout_ms));
+      if (res) {
+        auto end = std::chrono::steady_clock::now();
+        metric.recv_latency_ms.update((end - begin).count() / 1e6);
+      } else {
+        metric.recv_error_num.inc();
+        disconnect = true;
+        continue;
+      }
+    }
   }
+}
+
+cgo::Coroutine<void> udp_session(const Config& conf, std::string cli_ip, uint16_t cli_port, Metric& metric,
+                                 std::atomic<size_t>& wg) {
+  auto& ctx = cgo::this_coroutine_ctx();
+  auto sock = cgo::Socket::create(ctx, cgo::Socket::Protocol::UDP, cgo::Socket::AddressFamily::IPv4);
+  auto guard = cgo::defer([&sock, &wg]() {
+    sock.close();
+    wg.fetch_add(1);
+  });
+  sock.bind("0.0.0.0", 0);
+
+  auto ok = co_await sock.sendto("1", cli_ip, cli_port, std::chrono::milliseconds(conf.sock_timeout_ms));
+  if (!ok) {
+    co_return;
+  }
+  metric.conn_total_num.inc();
+
+  while (true) {
+    {
+      metric.recv_total_num.inc();
+      auto begin = std::chrono::steady_clock::now();
+      auto req = co_await sock.recvfrom(conf.req_pkg_nbytes, std::chrono::milliseconds(conf.sock_timeout_ms));
+      if (req) {
+        auto end = std::chrono::steady_clock::now();
+        metric.recv_latency_ms.update((end - begin).count() / 1e6);
+      } else {
+        metric.recv_error_num.inc();
+        co_return;
+      }
+    }
+    {
+      metric.send_total_num.inc();
+      auto res = generate_test_data(conf.res_pgk_nbytes);
+      auto begin = std::chrono::steady_clock::now();
+      auto ok = co_await sock.sendto(res, cli_ip, cli_port, std::chrono::milliseconds(conf.sock_timeout_ms));
+      if (ok) {
+        auto end = std::chrono::steady_clock::now();
+        metric.send_latency_ms.update((end - begin).count() / 1e6);
+      } else {
+        metric.send_error_num.inc();
+        co_return;
+      }
+    }
+  }
+}
+
+cgo::Coroutine<void> udp_server(const Config& conf, Metric& metric) {
+  auto& ctx = cgo::this_coroutine_ctx();
+  auto sock = cgo::Socket::create(ctx, cgo::Socket::Protocol::UDP, cgo::Socket::AddressFamily::IPv4);
+  auto guard = cgo::defer([&sock]() { sock.close(); });
+
+  sock.bind("0.0.0.0", conf.svr_port);
+
+  cgo::Context session_ctx;
+  size_t session_num = 0;
+  std::atomic<size_t> session_wg = 0;
+
+  session_ctx.start(conf.svr_ctx_threads);
+  auto session_guard = cgo::defer([&session_ctx, &session_wg, session_num]() {
+    while (session_wg.load() < session_num) {
+      std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
+    session_ctx.stop();
+  });
+
+  while (true) {
+    {
+      auto req = co_await sock.recvfrom(1);
+      if (!req) {
+        continue;
+      }
+      auto& [data, source] = *req;
+      auto& [cli_ip, cli_port] = source;
+
+      session_num++;
+      cgo::spawn(session_ctx, udp_session(conf, cli_ip, cli_port, metric, session_wg));
+    }
+  }
+}
+
+void udp_benchmark_test(Config& conf) {
+  Metric svr_metric;
+  cgo::Context svr_ctx;
+  svr_ctx.start(1);
+  cgo::spawn(svr_ctx, udp_server(conf, svr_metric));
+
+  Metric cli_metric;
+  cgo::Context cli_ctx;
+  cli_ctx.start(conf.cli_ctx_threads);
+  std::atomic<size_t> cli_wg = 0;
+  for (int i = 0; i < conf.cli_num; ++i) {
+    cgo::spawn(cli_ctx, udp_client(conf, cli_metric, cli_wg));
+  }
+
+  while (cli_wg.load() < conf.cli_num) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+  cli_ctx.stop();
+  svr_ctx.stop();
+
+  ::printf("UDP benchmark result:\n");
+  ::printf("Config:\n");
+  std::cout << conf.str();
+  // ::printf("Server:\n");
+  // std::cout << svr_metric.str();
+  ::printf("Client:\n");
+  std::cout << cli_metric.str();
+}
+
+TEST(socket, udp_bench) {
+  Config conf;
+  conf.test_duration_sec = 10;
+  conf.svr_port = 8081;
+  conf.sock_timeout_ms = 2000;
+  conf.cli_num = 2000;
+  conf.cli_interval_ms = 50;
+  conf.cli_ctx_threads = 1;
+  conf.svr_ctx_threads = 1;
+  conf.req_pkg_nbytes = 1024;
+  conf.res_pgk_nbytes = 1024;
+  udp_benchmark_test(conf);
 }
